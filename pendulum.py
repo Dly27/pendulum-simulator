929012cddde8f9dfa5c9cc5c54de1aaf1e3b6488
Dly27 <149893105+Dly27@users.noreply.github.com>
2024-02-15 12:43:53 +0000

    First commit

---
+++ pendulum.py
@@ -0,0 +1,66 @@
+import numpy as np
+import sys
+import pygame
+
+# Colours
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+def pendulum_equation(omega, theta, g, L):
+    alpha = -g / L * np.sin(theta)
+    return omega, alpha
+
+def pendulum_simulator(screen, clock, L, omega):
+    theta = np.pi / 4
+    g = 9.81
+    dt = 0.1
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        # Calculate new state
+        omega, alpha = pendulum_equation(omega, theta, g, L)
+        omega += alpha * dt
+        theta += omega * dt
+
+        # Convert polar into cartesian
+        x = int(L * np.sin(theta))
+        y = int(L * np.cos(theta))
+
+        # Fill screen
+        screen.fill(BLACK)
+
+        # Draw pendulum string
+        pygame.draw.line(screen, WHITE, (250, 100), (250 + x, 100 + y), 2)
+
+        # Draw pendulum bob
+        pygame.draw.circle(screen, RED, (250 + x, 100 + y), 20)
+
+        # Display update
+        pygame.display.flip()
+
+        # Framerate cap
+        clock.tick(60)
+
+def main():
+    # Initialize Pygame
+    pygame.init()
+
+    # Screen setup
+    screen_info = pygame.display.Info()
+    screen_width, screen_height = screen_info.current_w, screen_info.current_h
+    screen = pygame.display.set_mode((screen_width, screen_height-60))
+    pygame.display.set_caption("2D Pendulum Simulator")
+
+    # Clock setup
+    clock = pygame.time.Clock()
+
+    # Run simulation
+    pendulum_simulator(screen, clock, L=200, omega=0)
+
+if __name__ == "__main__":
+    main()
